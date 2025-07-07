835944104ec175732d808608d8f3e7c0a2f531ef
Turb0fish <219450503+Turb0fish@users.noreply.github.com>
2025-07-08 01:12:50 +0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-gcc -Imodules main.c modules/glad.c modules/image_loaders.c modules/render.c -o SharkPix -std=c11 -Wall -g \
-    -lSDL3 -lGL \
-    -lwebp -lheif -ltiff -ljpeg -ljxl -lspng \
-    -lpthread -lm -latomic
+gcc -Imodules main.c modules/glad.c modules/image_loaders.c modules/render.c -o SharkPix -std=c11 \
+	-lSDL3 -lSDL3_image -lGL \
+	-lwebp -lheif -ltiff -ljpeg -ljxl -lspng \
+	-lpthread -lm -latomic
